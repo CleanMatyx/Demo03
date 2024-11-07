dad82cb116362299e1cc66913dfdd767d81d5634
Matias <mtsbrr07@gmail.com>
2024-11-07 14:39:02 +0100

    Cambios

--- app/src/main/java/edu/matiasborra/demo03/MainViewModel.kt
+++ app/src/main/java/edu/matiasborra/demo03/MainViewModel.kt
@@ -1,4 +1,14 @@
 package edu.matiasborra.demo03
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private var _fragmentShowed: String? = null
+    val fragmentShowed: String?
+        get() = _fragmentShowed
+
+    fun setFragmentShowed(fragmentName: String) {
+        _fragmentShowed = fragmentName
+
+    }
 }
